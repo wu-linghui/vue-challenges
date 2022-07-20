6c9daac772d97246eba216c9976fa0a9e8c52a02
wu-linghui <1540996580@qq.com>
2022-07-20 23:41:48 +0800

    practice: losing-reactivity

---
+++ src/views/demo/losing-reactivity.vue
@@ -0,0 +1,32 @@
+<script setup lang="ts">
+import { reactive, toRefs } from "vue"
+
+function useCount() {
+  const state = reactive({
+    count: 0,
+  })
+
+  function update(value: number) {
+    state.count = value
+  }
+
+  return {
+    state: toRefs(state),
+    update,
+  }
+}
+
+// 确保解构不丢失响应性
+const { state: { count }, update } = useCount()
+
+</script>
+
+<template>
+  <div>
+    <p>
+      <span @click="update(count-1)">-</span>
+      {{ count }}
+      <span @click="update(count+1)">+</span>
+    </p>
+  </div>
+</template>

28942764ddef17df4690f9e5dfd6b2a4f6790810
wu-linghui <1540996580@qq.com>
2022-07-23 00:57:21 +0800

    practice: shallow-ref

---
+++ src/views/demo/shallow-ref.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+import { shallowRef, watch } from "vue"
+
+const state = shallowRef({ count: 1 })
+
+// 回调没被触发
+watch(state, () => {
+  console.log("State.count Updated")
+})
+
+/**
+ * 修改以下代码使watch回调被触发
+ *
+*/
+
+// 非递归监听， 不会触发watch
+state.value = {count: 2}
+
+</script>
+
+<template>
+  <div>
+    <p>
+      {{ state.count }}
+    </p>
+  </div>
+</template>

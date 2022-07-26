923ecfcc29b1a0f1dd69cf9cbca000e4ef22f4e9
wu-linghui <1540996580@qq.com>
2022-07-26 23:58:22 +0800

    practice: lifecycle

---
+++ src/views/demo/lifecycle.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+import { onMounted, inject, onUnmounted } from "vue"
+
+const timer: any = inject('timer')
+const count: any = inject('count')
+
+
+onMounted(() => {
+  // 切换子组件时, 定时器将不正常工作, 让我们来修复它 . 
+  timer.value = window.setInterval(() => {
+    count.value++
+  }, 1000)
+})
+
+onUnmounted(() => {
+  window.clearInterval(timer.value)
+})
+
+</script>
+
+<template>
+  <div>
+    <p>
+      Child Component: {{ count }}
+    </p>
+  </div>
+</template>
---
+++ src/views/father2.vue
@@ -0,0 +1,27 @@
+<template>
+  <life v-if="visible"></life>
+  <p>
+    <button @click="toggle">
+      Toggle Child Component
+    </button>
+  </p>
+</template>
+
+<script setup lang="ts">
+import { ref, provide } from 'vue';
+import life from './demo/lifecycle.vue';
+
+const count = ref(0);
+provide('count', count);
+
+const visible = ref(true)
+const timer = ref(null)
+provide('timer', timer)
+function toggle() {
+  visible.value = !visible.value
+}
+</script>
+
+<style scoped>
+
+</style>

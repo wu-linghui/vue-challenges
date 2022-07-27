7787fd4c00d695a028653e8be592002d30b5f2dc
wu-linghui <1540996580@qq.com>
2022-07-27 23:34:45 +0800

    practic：dom-portal

---
+++ src/views/demo/dom-portal.vue
@@ -0,0 +1,14 @@
+<script setup>
+
+const msg = "Hello World"
+
+</script>
+
+<template>
+  <!-- 将以下元素渲染成`body`的子元素 -->
+  <teleport to="body">
+    <span>{{ msg }}</span>
+  </teleport>
+</template>
+
+<!-- 文档：https://v3.cn.vuejs.org/guide/teleport.html#teleport -->

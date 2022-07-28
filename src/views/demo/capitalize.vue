5b9673864366b18b7b522708afce967d03240589
wu-linghui <1540996580@qq.com>
2022-07-28 23:42:52 +0800

    practice：Custom modifiers for 'v-model'

---
+++ src/views/demo/capitalize.vue
@@ -0,0 +1,21 @@
+<script setup>
+  import { ref, vModelText } from 'vue'
+  const _vModelText = vModelText
+
+  _vModelText.updated = (el, {value, modifiers: { capitalize }}) => {
+    if (capitalize && value.hasOwnProperty(0)) {
+      el.value = value[0].toUpperCase() + value.slice(1)
+    }
+  }
+
+  const v = ref('')
+</script>
+
+<template>
+  <input type="text" v-model.capitalize="v" />
+</template>
+
+<!-- 
+  github-issues: https://github.com/webfansplz/vuejs-challenges/issues/308
+  文档： https://v3.cn.vuejs.org/guide/custom-directive.html#%E7%AE%80%E4%BB%8B
+ -->

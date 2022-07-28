134d9148612bf4bb10341459588dbdbe1f7079f9
wu-linghui <1540996580@qq.com>
2022-07-28 23:04:02 +0800

    practice: useTOggle

---
+++ src/views/demo/useToggle.vue
@@ -0,0 +1,26 @@
+<script setup lang='ts'>
+import { Ref, ref, unref } from 'vue';
+
+/**
+ * 实现一个切换状态的可组合函数
+ * 确保该功能正常工作
+*/
+function useToggle(init: boolean) {
+  const state: Ref<boolean> = ref(init)
+  const toggle = () => {
+    state.value = !state.value
+  }
+
+  return [state, toggle]
+}
+
+const [state, toggle] = useToggle(false)
+
+</script>
+
+<template>
+  <p>State: {{ state ? 'ON' : 'OFF' }}</p>
+  <p @click="toggle">
+    Toggle state
+  </p>
+</template>

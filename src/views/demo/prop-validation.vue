6cf60a7a2ef12c74dcd5b69915e88bc8f795c30d
wu-linghui <1540996580@qq.com>
2022-07-29 22:19:28 +0800

    practice: prop-validation

---
+++ src/views/demo/prop-validation.vue
@@ -0,0 +1,24 @@
+<!-- 请验证Button组件的Prop类型 ，
+使它只接收: primary | ghost | dashed | link | text | default ，且默认值为default。 
+-->
+<script setup>
+defineProps({
+  type: {
+    type: String,
+    validator(val) {
+      return ['primary', 'ghost', 'dashed', 'link', 'text', 'default'].includes(val)
+    },
+    default() {
+      return 'default'
+    }
+  },
+})
+</script>
+
+<template>
+  <button>Button</button>
+</template>
+
+<!-- 
+  文档：https://v3.cn.vuejs.org/guide/component-props.html#prop-%E9%AA%8C%E8%AF%81
+ -->

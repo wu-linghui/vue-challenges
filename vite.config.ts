ff03be6068b0d8af48bd652162cceb074b8563df
wu-linghui <1540996580@qq.com>
2022-08-06 19:25:39 +0800

    chore: Update vite.config.ts so that Vue skips the default parsing of the Web Components

--- vite.config.ts
+++ vite.config.ts
@@ -8,7 +8,13 @@ import Inspector from 'vite-plugin-vue-inspector';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
-    vue(),
+    vue({
+      template: {
+        compilerOptions: {
+          isCustomElement: tag => tag.includes('-')
+        }
+      }
+    }),
     vueJsx(),
     Inspector({
       enabled: false,

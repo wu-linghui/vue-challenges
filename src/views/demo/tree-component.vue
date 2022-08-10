3beb692e883e7c7dee83f4365e5c9b26fe53b042
wu-linghui <1540996580@qq.com>
2022-08-10 23:21:31 +0800

    practice: tree-component

---
+++ src/views/demo/tree-component.vue
@@ -0,0 +1,21 @@
+<!-- 
+  在这个挑战中，你需要实现一个树组件，让我们开始吧。
+ -->
+<script setup lang="ts">
+interface TreeData {
+  key: string
+  title: string
+  children: TreeData[]
+}
+defineProps<{data: TreeData[]}>()
+</script>
+
+<template>
+  <!-- do something.... -->
+  <ul>
+    <template v-for="{ title, key, children } of data" :key="key">
+      <li>{{ title }}</li>
+      <tree-component v-if="Array.isArray(children)" :data="children"></tree-component>
+    </template>
+  </ul>
+</template>
